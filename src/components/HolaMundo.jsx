83619246315b578cca2efbebe7d78ea54b75f62c
Angel Gutierrez <gutierrezramosjoseangel@gmail.com>
2020-06-18 01:59:20 -0500

    Hello world Component added

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HolaMundo = () => {
+    const Hello = 'Hola mundo'
+    const isTrue = true
+
+    return (
+        <div className="HolaMundo">
+            <h1>{Hello}</h1>
+            <h2>Curso básico de React</h2>
+            <img src="https://i.imgur.com/lzxrKcK.jpg" alt="Code"/>
+            {isTrue ? <h1>Esto es verdadero</h1> : <h1>Soy Falso</h1>}
+            {isTrue && <h1>Soy verdadero</h1>}
+        </div>
+    )
+}
+
+export default HolaMundo
