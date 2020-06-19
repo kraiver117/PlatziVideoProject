dc38e3b6c5217ba5c3c3b561c88bde44c3229c55
Angel <bryanangel146@gmail.com>
2020-06-18 19:36:06 -0500

    Header finished and Eslint problems fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import HolaMundo from './components/demo/HolaMundo';
+import App from './containers/app'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HolaMundo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
