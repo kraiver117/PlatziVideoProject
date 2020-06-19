e7d3abb6c1519c5ec8cdb5659ca7018e2e2a43bf
Angel Gutierrez <gutierrezramosjoseangel@gmail.com>
2020-06-18 22:15:24 -0500

    SearchBar added

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 import '../assets/styles/app.scss';
+
 
 
 const app = () => {
     return (
         <div className="App">
             <Header></Header>  
+            <SearchBar></SearchBar>
         </div>
     );
 };
